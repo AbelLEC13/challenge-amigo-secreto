0a0c6377fdfec777190b1d873378d369da0f9fdc
Abel Leiva <abel.leiva@hotmail.com>
2025-07-29 09:30:53 -0600

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.ariaValueMax.trim();
+
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+}
+
+amigos.push(nombre);
+
+const lista = document.getElementById("listaAmigos");
